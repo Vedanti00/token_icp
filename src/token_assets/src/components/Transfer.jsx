d5e014d9fdc9c39e95ed1f8d00987523c6e1f063
Vedanti00 <vedantisinkar959@gmail.com>
2023-09-21 15:01:00 +0530

    final commit

---
+++ src/token_assets/src/components/Transfer.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { token } from "../../../declarations/token";
+import Principal from "@difinity/principal";
+
+function Transfer() {
+
+  const [recipientId, setId] = useState("");
+  const [amount, setAmount] = useState("");
+  const [isDisabled, setDisabled] = useState(false);
+  const [feedback, setFeedback] = useState("");
+  const [isHidden, setHidden] = useState(true);
+  
+  async function handleClick() {
+    setHidden(true);
+    setDisabled(true);
+    const recipients = Principal.fromText(recipientId);
+    const amountToTransfer = Number(amount);
+    const result = await token.transfer(recipients, amountToTransfer);
+    setFeedback(result);
+    setHidden(false);
+    setDisabled(false);
+  }
+
+  return (
+    <div className="window white">
+      <div className="transfer">
+        <fieldset>
+          <legend>To Account:</legend>
+          <ul>
+            <li>
+              <input
+                type="text"
+                id="transfer-to-id"
+                value={recipientId}
+                onChange={(e) => setId(e.target.value)}
+              />
+            </li>
+          </ul>
+        </fieldset>
+        <fieldset>
+          <legend>Amount:</legend>
+          <ul>
+            <li>
+              <input
+                type="number"
+                id="amount"
+                value={amount}
+                onChange={setAmount(e.target.value)}
+              />
+            </li>
+          </ul>
+        </fieldset>
+        <p className="trade-buttons">
+          <button id="btn-transfer" onClick={handleClick} disabled={isDisabled}>
+            Transfer
+          </button>
+        </p>
+        <p hidden={isHidden}>{feedback}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Transfer;
